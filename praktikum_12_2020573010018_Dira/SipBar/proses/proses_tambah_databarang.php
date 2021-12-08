d35dd643a99909004241ed0eadbafba1215dd48d
Dira Mahgfirah <90301070+diramhgfrh@users.noreply.github.com>
2021-12-08 15:16:16 +0700

    commit

---
+++ praktikum_12_2020573010018_Dira/SipBar/proses/proses_tambah_databarang.php
@@ -0,0 +1,16 @@
+<?php
+require "koneksi.php";
+$kd_brg = $_POST['kd_brg'];
+$nm_brg = $_POST['nm_brg'];
+$ket    = $_POST['ket'];
+$kondisi   = $_POST['kondisi'];
+
+$input = mysqli_query($conn,"INSERT INTO tb_barang (kode_barang, nama_barang, keterangan, kondisi) 
+values ('$kd_brg', '$nm_brg','$ket','$kondisi')");
+
+if ($input){
+    echo "<script>window.location='../databrg';</script>";
+}else{
+    echo "<script>alert('Mohon maaf data gagal di tambah');
+        window.location='../databrg';</script>";
+}
